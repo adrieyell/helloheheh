8afdcd7c939934f67e4db160a11d65d8e2915928
adie <117338817+adrieyell@users.noreply.github.com>
2024-02-14 00:09:01 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+function startQuestions() {
+    document.getElementById("questions").style.display = "block";
+  }
+  
+  function answerYes() {
+    document.getElementById("questions").style.display = "none";
+      document.getElementById("que").style.display = "none";
+      document.getElementById("init").innerText = "Thank you! 🥰";
+    document.getElementById("message").style.display = "block";
+    document.getElementById("messageText").innerText = "Thank you for saying yes :) I love you, baby! Happy Valentine's Day :)";
+    showConfetti()
+}
+  
+  function answerNo() {
+    document.getElementById("questions").style.display = "none";
+    document.getElementById("que").style.display = "none";
+    document.getElementById("init").innerText = "Oh no 😔";
+    document.getElementById("message").style.display = "block";
+    document.getElementById("messageText").innerText = "Why no :'(";
+    showConfetti()
+
+}
+  
+// Function to show confetti
+function showConfetti() {
+    const confettiSettings = {
+      target: 'confetti-canvas',
+      respawn: true,
+      colors: ['#ff0000', '#00ff00', '#0000ff'], 
+    };
+  
+    const confetti = new ConfettiGenerator(confettiSettings);
+    confetti.render();
+    setTimeout(() => {
+      confetti.clear();
+    }, 5000);
+  }
+  
